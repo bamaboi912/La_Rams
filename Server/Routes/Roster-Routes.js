14e72463ca8442c8bdd8ac8fb9bea04229067480
Kejuan Harrison <bamaboi912@icloud.com>
2023-02-24 12:47:53 -0600

    created routes file

---
+++ Server/Routes/Roster-Routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const router = express.Router();
+
+const Car = require("../Roster/RosterInformation");
+
+const rosterControllers = require("../Controllers/Roster-Controllers");
+
+
+
+//localhost:3002/roster/get
+router.get("/get", rosterControllers.getAllPlayers);
+
+//localhost:3002/roster/update
+router.post("/update", rosterControllers.addPlayer);
+
+//localhost:3002/roster/get:id
+router.get("/get/:id", rosterControllers.getByID);
+
+//localhost:3002/roster/update/:id
+router.put("/update/:id", rosterControllers.updatePlayer);
+
+//localhost:3002/roster/delete/:id
+router.delete("/delete/:id", rosterControllers.deletePlayer);
+
+module.exports = router;
